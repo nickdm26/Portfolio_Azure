e8f925fc8bc8709245b3045e310729ceb47a9a38
nickdm26 <nickdm26@gmail.com>
2021-06-27 16:37:37 +1200

    UnComment Import bootstrap .css file

--- portfolio_azure/src/index.js
+++ portfolio_azure/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter} from 'react-router-dom';
 import ReactDOM from 'react-dom';
-// import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
 
